7d3ba7818a122720b33699d60e49dbf0353e678c
kalisec2 <34760081+kalisec2@users.noreply.github.com>
2024-11-15 19:27:01 +0800

    Update routeros.sh

--- routeros.sh
+++ routeros.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://download.mikrotik.com/routeros/7.13.3/chr-7.13.3.img.zip -O chr.img.zip && \
+wget https://download.mikrotik.com/routeros/7.16.1/chr-7.16.1.img.zip -O chr.img.zip && \
 gunzip -c chr.img.zip > chr.img && \
 mount -o loop,offset=512 chr.img /mnt && \
 ADDRESS=`ip addr show ens18 | grep global | cut -d ' ' -f 6 | head -n 1` && \
